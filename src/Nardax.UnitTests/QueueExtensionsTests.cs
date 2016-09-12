63c7691c3fbd6ba4ae57b2da619099f0ab27974b
Hansi Blomberg <g-i-t-h-a-n-s-i@netcare.se>
2016-09-12 19:13:03 +0200

    minor: added unneccesary comment

--- src/Nardax.UnitTests/QueueExtensionsTests.cs
+++ src/Nardax.UnitTests/QueueExtensionsTests.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+  // just some random commenting for educational purposes
 namespace Nardax.Tests
 {
     [TestClass()]
