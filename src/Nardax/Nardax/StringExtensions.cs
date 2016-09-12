d50720a685f597b59a16f986b7afd93400d62e4f
Hansi Blomberg <g-i-t-h-a-n-s-i@netcare.se>
2016-09-12 19:38:59 +0200

    truncate right is back

--- src/Nardax/Nardax/StringExtensions.cs
+++ src/Nardax/Nardax/StringExtensions.cs
@@ -17,6 +17,14 @@ namespace Nardax
             return value.Substring(startIndex, maxLength);
         }
 
-     
+        public static string TruncateRight(this string value, int maxLength)
+        {
+            if (value.Length < maxLength)
+            {
+                return value;
+            }
+
+            return value.Substring(0, maxLength);
+        }
     }
 }
